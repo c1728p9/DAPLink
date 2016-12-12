4c29342898e78b86052fd6e5062ff284d0bb50ef
Russ Butler <c1728p9@gmail.com>
2016-12-11 18:17:53 -0600

    Improve script

--- test/slim_hid_test.py
+++ test/slim_hid_test.py
@@ -2,37 +2,55 @@ import threading
 import time
 import usb.core
 
+should_exit = False
+global_start_time = time.time()
+
+
+def _get_time():
+    return time.time() - global_start_time
+
 
 def rx_main(device, semaphore, rx_list):
-    while True:
-        semaphore.acquire()
-        data = device.read(0x81, 64, 10 * 1000)
-        rx_list.append(data)
+    global should_exit
+    try:
+        while True:
+            semaphore.acquire()
+            data = device.read(0x81, 64, 10 * 1000)
+            rx_list.append(data)
+    finally:
+        should_exit = True
 
 
 def test_main(thread_index, device):
-    board_id = device.serial_number
-    semaphore = threading.Semaphore(0)
-    rx_list = []
-    thread = threading.Thread(target=rx_main,
-                              args=(device, semaphore, rx_list))
-    thread.setDaemon(True)
-    thread.start()
+    global should_exit
+    try:
+        board_id = device.serial_number
+        semaphore = threading.Semaphore(0)
+        rx_list = []
+        thread = threading.Thread(target=rx_main,
+                                  args=(device, semaphore, rx_list))
+        thread.setDaemon(True)
+        thread.start()
 
-    loop_count = 0
-    while True:
-        if loop_count % 1000 == 0:
-            print("Thread %i loop %i board ID %s" %
-                  (thread_index, loop_count, board_id))
-        semaphore.release()
-        data = bytearray(64)
-        data[0] = 0x80
-        device.write(0x01, data, 10 * 1000)
-        while len(rx_list) == 0:
-            pass
-        resp = rx_list.pop(0)
-        assert resp[0] == 0x80
-        loop_count += 1
+        loop_count = 0
+        while True:
+            if loop_count % 1000 == 0:
+                print("Thread %i loop %i at %s board ID %s" %
+                      (thread_index, loop_count, time.strftime("%H:%M:%S"), board_id))
+            semaphore.release()
+            data = bytearray(64)
+            data[0] = 0x80
+            device.write(0x01, data, 10 * 1000)
+            while len(rx_list) == 0:
+                pass
+            resp = rx_list.pop(0)
+            assert resp[0] == 0x80
+            loop_count += 1
+    except:
+        print("Thread %i exception board %s" % (thread_index, board_id))
+        raise
+    finally:
+        should_exit = True
 
 
 def main():
@@ -44,8 +62,8 @@ def main():
         thread.start()
         thread_list.append(thread)
 
-    while True:
-        time.sleep(1000)
+    while not should_exit:
+        time.sleep(1)
 
 
 def _daplink_match(dev):
