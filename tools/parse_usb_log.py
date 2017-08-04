4cafe97eb30c6d7ce768c54321779ada250832a5
Russ Butler <russ.butler@arm.com>
2017-08-04 15:08:41 +0100

    backup tweaks

--- tools/parse_usb_log.py
+++ tools/parse_usb_log.py
@@ -1,8 +1,12 @@
-import json
 from struct import unpack, calcsize
 from pcapng import FileScanner
 from collections import namedtuple
+import logging
+logging.basicConfig(level=logging.ERROR)
 
+log = logging.getLogger(__name__)
+
+# Defined by Windows
 URB_FUNCTION = {
     "URB_FUNCTION_SELECT_CONFIGURATION": 0x0000,
     "URB_FUNCTION_SELECT_INTERFACE": 0x0001,
@@ -22,95 +26,191 @@ URB_FUNCTION = {
 }
 URB_FUNCTION_NUM_TO_STR = dict((value, key) for key, value in URB_FUNCTION.iteritems())
 
-def safe_list(obj):
-    new_list = []
-    itr = iter(obj)
-    try:
-        while True:
-            try:
-                print("Looping")
-                new_list.append(itr.next())
-            except ValueError:
-                print("Value error")
-                pass
-    except StopIteration:
-        print("Stop iteration")
-        pass
-
-    return new_list
-#USB pcap format - http://desowin.org/usbpcap/captureformat.html
-
-
+# Defined by USBPCAP - http://desowin.org/usbpcap/captureformat.html
 PCAP_HDR_FMT = "<HQIHBHHBBI"
 PCAP_HDR_SIZE = calcsize(PCAP_HDR_FMT)
 PcapHeader = namedtuple("PcapHeader", "header_len, irq_id, status, function,"
                         "info, bus, device, endpoint, transfer, data_length")
 
-def pcap_header(data):
-    return PcapHeader(*unpack(PCAP_HDR_FMT, data[:PCAP_HDR_SIZE]))
+PCAP_CONTROL_STAGE = {
+    "Setup": 0,
+    "Data": 1,
+    "Status": 2
+}
+NUM_TO_CONTROL_STAGE = dict((value, key) for key, value in PCAP_CONTROL_STAGE.iteritems())
+PCAP_TRANSFER_TO_TYPE = {
+    0: "Isochronous",
+    1: "Interrupt",
+    2: "Control",
+    3: "Bulk"
+}
+
+# Defined by pcapng - https://www.winpcap.org/ntar/draft/PCAP-DumpFileFormat.html
+PCAPNG_BLOCK = {
+    "INTERFACE_DESC": 0x00000001,
+    "ENHANCED_PACKET": 0x00000006,
+    "SECTION_HEADER": 0x0A0D0D0A
+}
+
+
+# Defines for this project
+
+
+USB_TYPE = set([
+    "Bulk",
+    "Interrupt",
+    "Control"
+])
+USB_DIR = set([
+    "Setup",
+    "In",
+    "Out",
+    "Status"
+])
+USBTransfer = namedtuple("USBTransfer", "bus, device, endpoint, type, dir, size_or_data")
+FMT_CBW = "<IIIBBB"
+SIZE_CBW = calcsize(FMT_CBW)
+CBWTransfer = namedtuple("CBWTransfer", "signature, tag, data_transfer_length, flags, lun, length")
+FMT_CSW = "<IIIB"
+SIZE_CSW = calcsize(FMT_CSW)
+CSWTransfer = namedtuple("CSWTransfer", "signature, tag, data_residue, status")
+SCSITransfer = namedtuple("SCSITransfer", "cbw, csw, data")
+
+
+def pcap_header_and_data(data):
+    hdr = PcapHeader(*unpack(PCAP_HDR_FMT, data[:PCAP_HDR_SIZE]))
+    return hdr, data[hdr.header_len:]
+
+
+def pcap_to_usb(data):
+    hdr = PcapHeader(*unpack(PCAP_HDR_FMT, data[:PCAP_HDR_SIZE]))
+    contents = data[hdr.header_len:]
+    ttype = PCAP_TRANSFER_TO_TYPE[hdr.transfer]
+    if ttype == "Control":
+        stage_num, = unpack("<B", data[PCAP_HDR_SIZE:PCAP_HDR_SIZE + 1])
+        direction = NUM_TO_CONTROL_STAGE[stage_num]
+    else:
+        direction = "In" if hdr.endpoint & 0x80 else "Out"
+    return USBTransfer(hdr.bus, hdr.device, hdr.endpoint & ~0x80, ttype, direction, data[hdr.header_len:])
+
+
+
+PCAPNG_LINK = {
+    249: pcap_to_usb
+    #220:
+}
+
+
+def pcapng_to_usb_transfers(filename):
+    with open(filename, "rb") as fp:
+        scanner = FileScanner(fp)
+        interfaces = None
+        usb_transfers = []
+        for block in scanner:
+            if block.magic_number == PCAPNG_BLOCK["SECTION_HEADER"]:
+                interfaces = []
+            elif block.magic_number == PCAPNG_BLOCK["INTERFACE_DESC"]:
+                print("Link type: %s" % block)
+                if block.link_type in PCAPNG_LINK:
+                    interfaces.append(PCAPNG_LINK[block.link_type])
+                else:
+                    log.error("Skipping interface!")
+            elif block.magic_number == PCAPNG_BLOCK["ENHANCED_PACKET"]:
+                usb_transfers.append(interfaces[block.interface_id](block.packet_data))
+    return usb_transfers
+
+
+def usb_to_scsi(xfers):
+    cbw = None
+    data = None
+    xfer_itr = iter(xfers)
+    scsi_list = []
+    try:
+        while True:
+
+            cbw = None
+            while cbw is None:
+                xfer = xfer_itr.next()
+                if xfer.dir != "Out":
+                    log.error("Wrong CBW direction: %s", xfer.dir)
+                    continue
+                if len(xfer.size_or_data) != 31:
+                    log.error("Wrong CBW size: %s", len(xfer.size_or_data))
+                    continue
+                if xfer.size_or_data[:4] != "USBC":
+                    log.error("Wrong CBW signature: %s", xfer.size_or_data[:4])
+                    continue
+                cbw = CBWTransfer(*unpack(FMT_CBW, xfer.size_or_data[:SIZE_CBW]))
+
+            data = None
+            if cbw.data_transfer_length > 0:
+                xfer = xfer_itr.next()
+                direction = "In" if cbw.flags & (1 << 7) else "Out"
+                if direction != xfer.dir:
+                    log.error("Wrong direction in data stage - got %s, expected %s", xfer.dir, direction)
+                data = xfer.size_or_data
+
+            xfer = xfer_itr.next()
+            if xfer.dir != "In":
+                log.error("Wrong transfer direction in CSW stage: %s", xfer.dir)
+            elif len(xfer.size_or_data) != 13:
+                log.error("Wrong CSW packet size: %s" % len(xfer.size_or_data))
+            csw = CSWTransfer(*unpack(FMT_CSW, xfer.size_or_data[:SIZE_CSW]))
+            scsi_list.append(SCSITransfer(cbw, csw, data))
+    except StopIteration:
+        pass
+    return scsi_list
+
+
+
 
 
 def main():
+    xfers = pcapng_to_usb_transfers("k64f_load.pcapng")
+    scsis = usb_to_scsi(xfer for xfer in xfers if xfer.endpoint == 2 and xfer.device == 10)
+    for scsi in scsis:
+        print("%s %s" % (scsi.cbw, scsi.csw))
+    # for xfer in xfers:
+    #     if xfer.endpoint != 2:
+    #         continue
+    #     if xfer.device != 10:
+    #         continue
+    #     print(xfer)
+    exit(0)
     with open('k64f_load.pcapng', 'rb') as fp:
-    #with open('usblog_2124.pcapng', 'rb') as fp:
-
         scanner = FileScanner(fp)
         blocks = list(scanner)
-        block_types = {type(block) for block in blocks}
-        function_set = set()
-        interface_set = set()
-        addr_sets = set()
-        transfer_set = set()
-        #link_type
+    block_types = {type(block) for block in blocks}
+    function_set = set()
+    interface_set = set()
+    addr_sets = set()
+    transfer_set = set()
+    #link_type
 
-        for block in (block for block in blocks if block.magic_number == 6):
-            hdr = pcap_header(block.packet_data)
-            function_set.add(hdr.function)
-            interface_set.add(block.interface_id)
-            addr_sets.add("%s.%s" % (hdr.device, hdr.endpoint))
-            transfer_set.add(hdr.transfer)
-        functions = sorted(list(function_set))
-        #interfaces =
-        functions_desc = [URB_FUNCTION_NUM_TO_STR[val] for val in functions]
-        print("Transfers: %s" % sorted(list(transfer_set)))
-        print("Addrs: %s" % sorted(list(addr_sets)))
-        print("Interface set: %s" % interface_set)
-        print("Used functions: %s" % functions_desc)
-            #print("pcap header: %s" % (pcap_header(block.packet_data),))
-        print("Function set: %s" % function_set)
-        print("Block 1507: %s" % blocks[1507])
-        print("Block types: %s" % block_types)
-        print("There are %s blocks" % len(blocks))
-        #for block in blocks:
-        #    print("Block %s" % block)
-    with open('k64f_load.json') as data_file:
-        data = json.load(data_file)
-    last_num = None
-    changes = []
-    for entry in data:
-        num = int(entry["_source"]["layers"]["frame"]["frame.number"])
-        if last_num is None or last_num + 1 != num:
-            changes.append((last_num, num))
-        last_num = num
+    for block in (block for block in blocks if block.magic_number == 6):
+        hdr = pcap_header(block.packet_data)
+        function_set.add(hdr.function)
+        interface_set.add(block.interface_id)
+        addr_sets.add("%s.%s" % (hdr.device, hdr.endpoint))
+        transfer_set.add(hdr.transfer)
+    functions = sorted(list(function_set))
+    #interfaces =
+    functions_desc = [URB_FUNCTION_NUM_TO_STR[val] for val in functions]
+    print("Transfers: %s" % sorted(list(transfer_set)))
+    print("Addrs: %s" % sorted(list(addr_sets)))
+    print("Interface set: %s" % interface_set)
+    print("Used functions: %s" % functions_desc)
+        #print("pcap header: %s" % (pcap_header(block.packet_data),))
+    print("Function set: %s" % function_set)
+    print("Block 1507: %s" % blocks[1507])
+    print("Block types: %s" % block_types)
+    print("There are %s blocks" % len(blocks))
 
-    print("Last number %s" % last_num)
-    print("There are %s changes" % len(changes))
-    for old, new in changes:
-        print("%s -> %s" % (old, new))
-
-    #
-    # "_source": {
-    #     "layers": {
-    #         "frame": {
-    #             "frame.interface_id": "0",
-    #             "frame.encap_type": "152",
-    #             "frame.time": "Aug  2, 2017 09:23:56.319101000 GMT Daylight Time",
-    #             "frame.offset_shift": "0.000000000",
-    #             "frame.time_epoch": "1501662236.319101000",
-    #             "frame.time_delta": "0.008002000",
-    #             "frame.time_delta_displayed": "0.008002000",
-    #             "frame.time_relative": "3.411886000",
-    #             "frame.number": "51",
+#
+# def find_daplink_usb(usb_sequence):
+#
+#     "USBC" - send
+#     "USBS" - recv
 
 
 if __name__ == "__main__":
